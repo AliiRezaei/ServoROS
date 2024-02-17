b74de4363daaaaff3feedf554a3b976cf20e7399
aliirezaei <alii.rezzaei@gmail.com>
2024-02-17 20:21:15 +0330

    add comments to thepublisher node

--- arduino/servo_ros_publisher/servo_ros_publisher.ino
+++ arduino/servo_ros_publisher/servo_ros_publisher.ino
@@ -2,48 +2,71 @@
 #include <std_msgs/Int16.h>
 #include <Servo.h>
 
-ros::NodeHandle  nh;
-Servo myservo;
 
-std_msgs::Int16 position;
-ros::Publisher PosePublisher("servo_pose", &position);
+// global variables :
+ros::NodeHandle  nh;                                   // node handler
+Servo micro_servo;                                     // micro servo onject
 
-int pos = 0;
-int servo_pin = 9;
+std_msgs::Int16 position;                              // publisher var
+ros::Publisher PosePublisher("servo_pose", &position); // position publisher
 
+int pos = 0;                                           // micro servo initial position
+int servo_pin = 9;                                     // data attached pin
+// end of global variables.
 
-void setup() {
+void setup() 
+{
 
-
-  myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
-  nh.initNode();
-  nh.advertise(PosePublisher);
-
-}
-
-
-void loop() {
-
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-
-    // in steps of 1 degree
-
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    position.data = pos;
-    PosePublisher.publish(&position);
-    nh.spinOnce();
-    delay(15);                       // waits 15ms for the servo to reach the position
-
-  }
-
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    position.data = pos;
-    PosePublisher.publish(&position);
-    nh.spinOnce();
-    delay(15);                       // waits 15ms for the servo to reach the position
-
-  }
+  micro_servo.attach(servo_pin);  // configue micro servo object
+  nh.initNode();                  // initialize node handler
+  nh.advertise(PosePublisher);    // advertise position publisher
 
 }
+
+
+void loop()
+{
+
+  // servo start position --> 0   degrees
+	// servo start position --> 180 degrees
+	// direction --> counter clockwise
+  for (pos = 0; pos <= 180; pos += 1) 
+  {
+    // write current position :
+    micro_servo.write(pos);
+    
+    // assigning loop var to the publishher var :
+    position.data = pos;
+    
+    // publish current position :
+    PosePublisher.publish(&position);
+    
+    // ros spin :
+    nh.spinOnce();
+
+    // get some delay :
+    delay(15);
+  } // end of for loop
+
+  // servo start position --> 180 degrees
+  // servo start position --> 0   degrees
+	// direction --> clockwise
+  for (pos = 180; pos >= 0; pos -= 1)
+  {
+    // write current position :
+    micro_servo.write(pos);
+
+    // assigning loop var to the publishher var :
+    position.data = pos;
+
+    // publish current position :
+    PosePublisher.publish(&position);
+    
+    // ros spin :
+    nh.spinOnce();
+
+    // get some delay :
+    delay(15);
+  } // end of for loop
+
+} // end of void loop
