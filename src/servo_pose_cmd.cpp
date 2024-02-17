4aa9699b856e2c4a2173ac72b82d72128f8662fe
aliirezaei <alii.rezzaei@gmail.com>
2024-02-17 19:53:57 +0330

    add some comments

--- src/servo_pose_cmd.cpp
+++ src/servo_pose_cmd.cpp
@@ -19,28 +19,49 @@ int main(int argc, char **argv)
 	// while ros master is connected, publish servo position :
 	while(ros::ok())
 	{
-		std_msgs::Int16 position;
-		int pos = 0;
+		std_msgs::Int16 position; // for publishing
+		int pos = 0;              // for loop
 
+		// servo start position --> 0   degrees
+		// servo start position --> 180 degrees
+		// direction --> counter clockwise
 		for (pos = 0; pos <= 180; pos += 1)
 	    {
+			// assigning loop var to the publishher var :
 			position.data = pos;
-			PosePublisher.publish(position);
-			ROS_INFO("Servo Position is : %d", position.data);
-			rate.sleep();	
-		}  
 
+			// publish current position :
+			PosePublisher.publish(position);
+
+			// display servo angle in terminal :
+			ROS_INFO("Servo Position is : %d degrees", position.data);
+
+			// get some delay :
+			rate.sleep();	
+		} // end of for 
+
+		// servo start position --> 180 degrees
+		// servo start position --> 0   degrees
+		// direction --> clockwise
 		for (pos = 180; pos >= 0; pos -= 1)
         {
+			// assigning loop var to the publishher var :
             position.data = pos;
+
+			// publish current position :
             PosePublisher.publish(position);
-			ROS_INFO("Servo Position is : %d", position.data);
-            rate.sleep();
-        }
-	}
+
+			// display servo angle in terminal :
+			ROS_INFO("Servo Position is : %d degrees", position.data);
+            
+			// get some delay :
+			rate.sleep();
+        } // end of for
+	} // end of while
 
 	return 0;
-}
+
+} // end of main
 
 
 
